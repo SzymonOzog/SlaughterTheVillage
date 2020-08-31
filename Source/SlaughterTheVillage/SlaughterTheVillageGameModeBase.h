9189c54d7efba31090871e2c2d8f45d4493b86b3
Szymon Ozog <szymon.ozog@gmail.com>
2020-08-31 07:51:32 +0200

    Initial commit

---
+++ Source/SlaughterTheVillage/SlaughterTheVillageGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SlaughterTheVillageGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SLAUGHTERTHEVILLAGE_API ASlaughterTheVillageGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
