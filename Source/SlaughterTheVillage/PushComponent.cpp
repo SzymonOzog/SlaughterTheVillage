6eb547f6e2dab5ee8a6e55f43f11a483b88eeff3
Szymon Ozog <szymon.ozog@gmail.com>
2020-09-04 08:28:49 +0200

    Created a push component

---
+++ Source/SlaughterTheVillage/PushComponent.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PushComponent.h"
+#include "GameFramework/Actor.h"
+UPushComponent::UPushComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UPushComponent::SetDestination(FVector _Destination)
+{
+	Destination = _Destination;
+}
+
+void UPushComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void UPushComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	FVector CurrentLocation = GetOwner()->GetTargetLocation();
+	FVector NewLocation = FMath::VInterpTo(CurrentLocation, Destination, DeltaTime, 10.0f);
+	GetOwner()->SetActorLocation(NewLocation, false);
+	if (CurrentLocation == Destination)
+		DestroyComponent();
+}
