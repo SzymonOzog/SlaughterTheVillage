c724685484d70466db05049ba857a21bda54263b
Szymon Ozog <szymon.ozog@gmail.com>
2020-09-04 07:07:45 +0200

    Created BaseMissile class

---
+++ Source/SlaughterTheVillage/BaseMissile.cpp
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaseMissile.h"
+#include "BaseCharacter.h"
+#include "Components/StaticMeshComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+// Sets default values
+ABaseMissile::ABaseMissile()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	MissileMesh = CreateDefaultSubobject<UStaticMeshComponent>("MissileMesh");
+	SetRootComponent(MissileMesh);
+
+	MissileMovement = CreateDefaultSubobject<UProjectileMovementComponent>("MissileMovement");
+	MissileMovement->InitialSpeed = MissileSpeed;
+	MissileMovement->MaxSpeed = MissileSpeed;
+	MissileMovement->ProjectileGravityScale = GravityScale;
+
+	OnActorHit.AddDynamic(this, &ABaseMissile::OnMissileHit);
+}
+
+// Called when the game starts or when spawned
+void ABaseMissile::BeginPlay()
+{
+	Super::BeginPlay();
+	OwningPlayer = Cast<ABaseCharacter>(GetOwner());
+}
+
+// Called every frame
+void ABaseMissile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	//if player is holding LMB change velocity according to his location
+	if (OwningPlayer && OwningPlayer->IsCharacterAttacking())
+	{
+		FVector LookingDirection = OwningPlayer->GetControlRotation().Vector();
+		FVector TranslatedLookingDirection = UKismetMathLibrary::InverseTransformDirection(GetTransform(), LookingDirection);
+		MissileMovement->SetVelocityInLocalSpace(TranslatedLookingDirection * MissileSpeed);
+	}
+}
+
+void ABaseMissile::OnMissileHit(AActor* SelfActor, AActor* OtherActor, FVector NormalImpulse, const FHitResult& Hit)
+{
+	applySpecialEffect(SelfActor, OtherActor, NormalImpulse, Hit);
+	if (ABaseCharacter* Character = Cast<ABaseCharacter>(OtherActor))
+	{
+		Character->TakeDamage(1.0f, FDamageEvent(), GetInstigator()->GetController(), GetOwner());
+	}
+	Destroy();
+}
+
